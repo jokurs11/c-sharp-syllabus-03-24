d8f11d43108bb5240b9e4fb6e893e073ed0eb79e
Rihards <60659691+jokurs11@users.noreply.github.com>
2024-08-20 20:53:15 +0300

    Update Program.cs

--- csharp-basics/exercises/FlowOfControl/LargestNumber/Program.cs
+++ csharp-basics/exercises/FlowOfControl/LargestNumber/Program.cs
@@ -8,34 +8,26 @@ namespace LargestNumber
         {
             Console.WriteLine("Input the 1st number: ");
             var input1 = Console.ReadLine();
-
             Console.WriteLine("Input the 2nd number: ");
             var input2 = Console.ReadLine();
-
             Console.WriteLine("Input the 3rd number: ");
             var input3 = Console.ReadLine();
 
-            int parsedInt1 = int.Parse(input1);
-
-            int parsedInt2 = int.Parse(input2);
-
-            int parsedInt3 = int.Parse(input3);
-
-
+            int ParsedInput1 = int.Parse(input1);
+            int ParsedInput2 = int.Parse(input2);
+            int ParsedInput3 = int.Parse(input3);
 
             if (parsedInt1 > parsedInt2 && parsedInt1 > parsedInt3)
             {
-                Console.WriteLine("Largest number is first: " + parsedInt1);
-
+                Console.WriteLine("Largest number is first: " + parsedInput1);
             } 
             else if (parsedInt2 > parsedInt1 && parsedInt2 > parsedInt3)
             {
-                Console.WriteLine("Largest number is second: " + parsedInt2);
+                Console.WriteLine("Largest number is second: " + parsedIput2);
             }
             else
             {
-                Console.WriteLine("Largest number is third: " + parsedInt3);
-
+                Console.WriteLine("Largest number is third: " + parsedInput3);
             }
 
         }
