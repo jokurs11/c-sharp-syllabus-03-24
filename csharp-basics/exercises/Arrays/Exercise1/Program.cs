5e4e05d7d85fa2e022c19c1ae2afe9919d61af9f
Rihards <hextorr@gmail.com>
2024-08-20 23:53:15 +0300

    init

--- csharp-basics/exercises/Arrays/Exercise1/Program.cs
+++ csharp-basics/exercises/Arrays/Exercise1/Program.cs
@@ -23,18 +23,21 @@ namespace Exercise1
                 "C Programming",
                 "C++"
             };
-            
-            
-            //fixme
+
+
+                
             Console.WriteLine("Original numeric array : " + string.Join("," , myArray1));
-            //........... //Sort array
             
-            Console.WriteLine("Sorted numeric array : " + string.Join("," , myArray1));
-    
+            Array.Sort(myArray1);
+
+            Console.WriteLine("Sorted numeric array : " + string.Join(",", myArray1));
+
             Console.WriteLine("Original string array : " + string.Join("," , myArray2));
-            //......... //Sort array
             
-            Console.WriteLine("Sorted string array : " + string.Join("," , myArray2));
+            Array.Sort(myArray2);
+
+            Console.WriteLine("Sorted string array : " + string.Join(",", myArray2));
+
             //*/
             Console.ReadKey();
         }
